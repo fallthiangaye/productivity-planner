29c9ac0921655107e9c6159edcade42b0ac3291e
thiangaye <thiangaye.fall@uadb.edu.sn>
2024-12-06 11:15:58 +0000

    chore(firebase) : add variables environments

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCyJ1AjDugdb3gqdHYkq-dypvfZWqziYCk',
+    authDomain: 'productivity-planner-dev-c2243.firebaseapp.com',
+    projectId: 'productivity-planner-dev-c2243',
+    storageBucket: 'productivity-planner-dev-c2243.firebasestorage.app',
+    messagingSenderId: '155097786494',
+    appId: '1:155097786494:web:762a299113c6865664902f',
+  },
+};
